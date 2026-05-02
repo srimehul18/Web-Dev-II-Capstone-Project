4fb5cf7dd49265bdd6182bd13b28e25438e6baa4
Mehul Srivastava <srimehul18@gmail.com>
2026-05-02 19:53:30 +0530

    removed unnecessary pages

--- HealthDashboard/src/App.jsx
+++ HealthDashboard/src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./Pages/Home";
 import Doctors from "./Pages/Doctors";
 import Appointments from "./Pages/Appointments";
 import Dashboard from "./Pages/Dashboard";
-import Login from "./Pages/Login";
 
 
 export default function App() {
@@ -16,7 +15,6 @@ export default function App() {
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/appointments" element={<Appointments />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
